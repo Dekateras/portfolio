afb4846b3e0c732e66effbfd2849c05d6c6c13ce
Tom <thomasm229@hotmail.com>
2022-09-14 22:19:20 +1000

    Updated links JavaScript file Swapped link location to bottom-container div

--- scripts/basicLinks.js
+++ scripts/basicLinks.js
@@ -1,12 +1,20 @@
 
-var elementID = 'links';
+var elementID = "links";
 
 function loadLinks() {
 
   document.getElementById(elementID).innerHTML = "";
 
+  addLink("https://www.linkedin.com", "LinkedIn");
+  addLink("https://twitter.com", "Twitter");
+  addLinkLast("https://www.appbrewery.co", "App Brewery");
+
 }
 
-function addLink (htmlPage, pageName){
-  document.getElementById(elementID).innerHTML += "<a href=\"" + htmlPage +"\">" + pageName + "</a><br>";
+function addLink (link, pageName){
+  document.getElementById(elementID).innerHTML += "<a href=\"" + link +"\">" + pageName + "</a> | ";
+}
+
+function addLinkLast(link, pageName){
+  document.getElementById(elementID).innerHTML += "<a href=\"" + link +"\">" + pageName + "</a>";
 }
